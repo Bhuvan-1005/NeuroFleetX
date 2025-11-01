67eb84fcfa8a980330c80aafa932d8fc504de978
Kitkat1005 <bhuvaneshwar2006@gmail.com>
2025-11-01 23:35:53 +0530

    Remove all files from repository

--- Backend-Java/src/main/java/com/neurofleetx/repository/VehicleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.neurofleetx.repository;
-
-import com.neurofleetx.entity.Vehicle;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VehicleRepository extends MongoRepository<Vehicle, String> {
-}
